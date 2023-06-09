75de7311576ea684c4498afd7d5d7dd1dbe415bd
Andrei Trunkin <inboxavt@gmail.com>
2023-06-09 11:31:51 +0200

    Version 5 for testing (localhost)

--- frontend/src/pages/components/messages_block.jsx
+++ frontend/src/pages/components/messages_block.jsx
@@ -72,41 +72,46 @@ const MessagesBlock = () => {
             });
             resetForm();
           }}>
-          <div className="col-12 m-0">
-            <Form>
-              <div className="input-group">
-                <Field
-                  autoFocus
-                  name="body"
-                  type="text"
-                  className="form-control border-primary text-primary"
-                  placeholder={t('messageField')}
-                  aria-label={t('ariaLabel')}
-                  aria-describedby="button-addon2"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title={t('fieldTitle')}
-                  value=""
-                />
-                <button
-                  className="btn btn-primary"
-                  type="submit"
-                  id="button-addon2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                    fill="currentColor">
-                    <path
-                      fillRule="evenodd"
-                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
-                  </svg>
-                  <span className="visually-hidden">{t('messageSend')}</span>
-                </button>
-              </div>
-            </Form>
-          </div>
+          {({ isSubmitting }) => (
+            <div className="col-12 m-0">
+              <Form>
+                <div className="input-group">
+                  <Field
+                    autoFocus
+                    name="body"
+                    type="text"
+                    className="form-control border-primary text-primary"
+                    placeholder={t('messageField')}
+                    aria-label={t('ariaLabel')}
+                    aria-describedby="button-addon2"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title={t('fieldTitle')}
+                    value=""
+                  />
+                  <button
+                    className="btn btn-primary"
+                    type="submit"
+                    id="button-addon2"
+                    disabled={isSubmitting}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      width="20"
+                      height="20"
+                      fill="currentColor">
+                      <path
+                        fillRule="evenodd"
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"></path>
+                    </svg>
+                    <label htmlFor="button-addon2" className="visually-hidden">
+                      {t('messageSend')}
+                    </label>
+                  </button>
+                </div>
+              </Form>
+            </div>
+          )}
         </Formik>
       </div>
     </div>
